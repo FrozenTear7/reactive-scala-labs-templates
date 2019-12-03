a92a351e893c18adfa22e1b71eec986ff441e61a
Paweł Mendroch <pawelmendroch7@gmail.com>
2019-12-03 19:05:25 +0100

    Add lab5

---
+++ src/main/scala/EShop/lab5/Payment.scala
@@ -0,0 +1,59 @@
+package EShop.lab5
+
+import EShop.lab2.Checkout
+import EShop.lab3.Payment.{DoPayment, PaymentConfirmed}
+import EShop.lab5.Payment.{PaymentRejected, PaymentRestarted}
+import EShop.lab5.PaymentService.PaymentSucceeded
+import akka.actor.SupervisorStrategy.{Restart, Stop}
+import akka.actor.{Actor, ActorLogging, ActorRef, OneForOneStrategy, Props, Terminated}
+import akka.stream.StreamTcpException
+
+import scala.concurrent.duration._
+
+object Payment {
+  case object PaymentRejected
+  case object PaymentRestarted
+
+  def props(method: String, orderManager: ActorRef, checkout: ActorRef) =
+    Props(new Payment(method, orderManager, checkout))
+}
+
+class Payment(
+  method: String,
+  orderManager: ActorRef,
+  checkout: ActorRef
+) extends Actor
+  with ActorLogging {
+
+  override def receive: Receive = {
+    case DoPayment =>
+      val worker = context.actorOf(PaymentService.props(method, self))
+      context.watch(worker)
+    case PaymentSucceeded =>
+      orderManager ! PaymentConfirmed
+      checkout ! Checkout.ReceivePayment
+    case Terminated(_) => notifyAboutRejection()
+  }
+
+  override val supervisorStrategy: OneForOneStrategy =
+    OneForOneStrategy(maxNrOfRetries = 3, withinTimeRange = 1.seconds) {
+      case _: PaymentService.PaymentClientError | _: StreamTcpException =>
+        notifyAboutRejection()
+        Stop
+      case _: PaymentService.PaymentServerError =>
+        notifyAboutRestart()
+        Restart
+    }
+
+  //please use this one to notify when supervised actor was stoped
+  private def notifyAboutRejection(): Unit = {
+    orderManager ! PaymentRejected
+    checkout ! PaymentRejected
+  }
+
+  //please use this one to notify when supervised actor was restarted
+  private def notifyAboutRestart(): Unit = {
+    orderManager ! PaymentRestarted
+    checkout ! PaymentRestarted
+  }
+}
